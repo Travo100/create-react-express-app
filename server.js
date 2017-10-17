61f20469c38eb6011317c8c789c13c687e055077
Travo100 <Travo100@gmail.com>
2017-10-17 16:29:50 -0700

    Added in mongo and mongoose starter code

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const Test = require("./models/test");
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/myDatabase",
+  {
+    useMongoClient: true
+  }
+);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("/healthcheck", function(req,res){
-  res.json({"success": true, status: 200})
+// just a dummy GET route on our Test model
+app.get("/data", function(req,res){
+  Test.find((err, data) => { 
+    if(err) throw err; 
+    res.json(data);
+  });
+});
+
+// just a post on our Test model
+app.post("/new", function(req, res){
+  Test.create(req.body, (err, data) => {
+    if(err) throw err;    
+    res.json(data);
+  });
 });
 
 // Send every request to the React app
