38b402790a5993839eec3a2211fe309082eb2276
Travo100 <Travo100@gmail.com>
2017-10-17 14:36:38 -0700

    Added body parser

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
