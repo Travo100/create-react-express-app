bd8a57b81dc938778ef27655c58c76479626c3b8
John Desrosiers <johndrunner@yahoo.com>
2017-10-21 14:59:17 -0700

    added POST /places/details route

--- controllers/placesAPI.js
+++ controllers/placesAPI.js
@@ -2,29 +2,62 @@ const axios = require('axios');
 
 // Controller for the google places api
 const key = 'AIzaSyAHO4q-p7lzrf6zVUXcifYSEDwAz4p5Dds';
-const textSearchUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
+const placesHostUrl = 'https://maps.googleapis.com/maps/api/place/';
 
-module.exports = {
-  textSearch: (req, res) => {
-    const params = {
-      query: req.params.query,
-      key,
-    };
+// Returns an axios promise for a request sent to the google places
+// api at the details endpoint
+function getDetailsById(placeid) {
+  const detailsUrl = `${placesHostUrl}details/json`;
+  const params = { key, placeid };
 
-    return axios.get(textSearchUrl, { params })
-      .then((response) => {
-        const places = response.data.results || [];
-        const { status } = response.data;
+  // returns axios promise
+  return axios.get(detailsUrl, { params });
+}
 
-        // send array of up to 5 results from google places api
-        if (status === 'OK' || status === 'ZERO_RESULTS ') {
-          if (places.length < 5) return res.json(places);
-          return res.json(places.slice(0, 5));
-        }
+// Sends the details from google places api for a specific
+// google placeid. Expects req.body to be json.
+// View the response format here: https://developers.google.com/places/web-service/details#PlaceDetailsResponses
+function getPlaceDetails(req, res) {
+  getDetailsById(req.body.placeid)
+    .then((response) => {
+      const details = response.data.result;
+      const { status } = response.data;
 
-        // send google status code
-        return res.json({ status });
-      })
-      .catch(error => res.json(error));
-  },
-};
+      // return details if google status message is 'OK'
+      if (status === 'OK') return res.json(details);
+
+      // return google status for all other responses from
+      // the google api
+      return res.json({ status });
+    })
+    .catch(error => res.status(400).json(error));
+}
+
+// Sends an array of up to five results from a text search
+// on the google places api.
+// View the response format here: https://developers.google.com/places/web-service/search#PlaceSearchResponses
+function textSearch(req, res) {
+  const textSearchUrl = `${placesHostUrl}textsearch/json`;
+  const params = {
+    query: req.params.query,
+    key,
+  };
+
+  axios.get(textSearchUrl, { params })
+    .then((response) => {
+      const places = response.data.results || [];
+      const { status } = response.data;
+
+      // send array of up to 5 results from google places api
+      if (status === 'OK' || status === 'ZERO_RESULTS ') {
+        if (places.length < 5) return res.json(places);
+        return res.json(places.slice(0, 5));
+      }
+
+      // send google status code
+      return res.json({ status });
+    })
+    .catch(error => res.status.json(error));
+}
+
+module.exports = { getPlaceDetails, textSearch };
--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,8 @@
 const routes = require('express').Router();
-const { textSearch } = require('../controllers/placesAPI');
+const bodyParser = require('body-parser');
+const { getPlaceDetails, textSearch } = require('../controllers/placesAPI');
 
 routes.get('/search/:query', textSearch);
+routes.use('/details', bodyParser.json())
+  .post('/details', getPlaceDetails);
 module.exports = routes;
