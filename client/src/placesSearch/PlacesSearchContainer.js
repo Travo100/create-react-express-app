aaad5abf2d11442338f42229311f01233d1ee4ff
John Desrosiers <johndrunner@yahoo.com>
2017-10-22 07:10:08 -0700

    added PlacesSearchContainer - renders simple text

---
+++ client/src/placesSearch/PlacesSearchContainer.js
@@ -0,0 +1,13 @@
+// Top Level Container for the Google Places Search
+
+// This container component handles the behavior of the google search.
+
+// DEPENDENCIES
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PlacesSearchContainer extends Component {
+  render() { return <div>PlacesSearchContainer</div>; }
+}
+
+export default PlacesSearchContainer;
