c7a173a090944eba62da95a39ea3a7820d84a516
John Desrosiers <johndrunner@yahoo.com>
2017-10-23 07:53:52 -0700

    added getDetails method to placesAPI.js

--- client/src/lib/placesAPI.js
+++ client/src/lib/placesAPI.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
-// Request places from server.
+// Request place details. Returns promise for place details object from googlePlaces API.
+function getDetails ( placeid ) {
+    return axios.post( '/places/details', { placeid })
+        .then( ( result ) => {
+            if ( result.statusText === 'OK' ) {
+                return result.data;
+            }
+            // TODO add error handling
+            console.log( result.statusText );
+            return null;
+        })
+        .catch( ( error ) => {
+            console.log( error );
+            return [];
+        });
+}
+
+// Request places from server. Returns promise for array of results from the googlePlaces search API.
 function textSearch ( queryString ) {
     return axios.get( `/places/search/${queryString}` )
         .then( ( result ) => {
@@ -14,9 +31,9 @@ function textSearch ( queryString ) {
 
         // TODO add error handling
         .catch( ( error ) => {
-            console.log(error);
+            console.log( error );
             return [];
         });
 }
 
-export default { textSearch };
+export default { getDetails, textSearch };
