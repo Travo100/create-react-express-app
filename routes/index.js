ec3a80b3068d4d902473c6cf747f9468ac1caea4
John Desrosiers <johndrunner@yahoo.com>
2017-10-24 06:33:57 -0700

    fixed routing to trips api with comments to remove later

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ const router = require( 'express' ).Router();
 const places = require( './places' );
 const trips = require( './trips' );
 
-// const apiRoutes = require("./api");
 // API Routes
-// router.use("/api", apiRoutes);
-// If no API routes are hit, send the React app
+router.use( '/api/trips', trips );
 router.use( '/places', places );
+// If no API routes are hit, send the React app
 router.use( ( req, res ) => {
     if ( process.env.NODE_ENV === 'production' ) {
         res.sendFile( path.join( __dirname, '../client/build/index.html' ) );
@@ -15,10 +14,16 @@ router.use( ( req, res ) => {
         res.sendFile( path.join( __dirname, '../client/public/index.html' ) );
     }
 } );
+// any routes mounted below this line are unreachable
+// ==================================================================================
 
-router.use( '/api/trips', trips );
-router.use( function ( req, res ) {
-    res.sendFile( path.join( __dirname, '../client/build/index.html' ) );
-} );
+// REMOVE BEFORE MERGING WITH THE DEV BRANCH
+// ==================================================================================
+// api/trips route needs to be mounted BEFORE the catch all route for the React client
+// router.use( '/api/trips', trips );
+// router.use( function ( req, res ) {
+//     res.sendFile( path.join( __dirname, '../client/build/index.html' ) );
+// } );
+// ==================================================================================
 
 module.exports = router;
