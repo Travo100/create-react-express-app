4c50b26e4cc0af73cb8781de442530a4b4e7ebd0
Jasmine Kim <jasminepkim@gmail.com>
2017-10-24 13:58:21 -0700

    all trips routes working

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -20,6 +20,13 @@ module.exports = {
             .findOneAndUpdate( { '_id': req.params.id }, req.body )
             .then( dbModel => res.json( dbModel ) )
             .catch( err => res.status( 422 ).json( err ) );
+    },
+    'remove': function ( req, res ) {
+        Trip
+            .findById( { '_id': req.params.id } )
+            .then( dbModel => dbModel.remove() )
+            .then( dbModel => res.json( dbModel ) )
+            .catch( err => res.status( 422 ).json( err ) );
     }
 }
 ;
--- routes/trips.js
+++ routes/trips.js
@@ -2,20 +2,25 @@ const bodyParser = require( 'body-parser' );
 const router = require( 'express' ).Router();
 const tripsController = require( '../controllers/tripsController' );
 
+router.use( '/*', bodyParser.json() );
+
 // Route matches with "api/trips/new"
+// To create a new trip
 router
-    .use( '/new', bodyParser.json() )
     .route( '/new' )
     .post( tripsController.create );
 
 // Route matches with "api/trips/"
+// To fetch all trips from the database
 router
     .route( '/' )
     .get( tripsController.findAll );
 
 // Route matches with "api/trips/:id"
+// To update and delete specific trips
 router
     .route( '/:id' )
-    .put( tripsController.update );
+    .put( tripsController.update )
+    .delete( tripsController.remove );
 
 module.exports = router;
