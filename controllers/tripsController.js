10bd3f6416cf37fd3af0df3f117bf89536c26950
John Desrosiers <johndrunner@yahoo.com>
2017-10-24 06:42:32 -0700

    added comments about server crash

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,8 +1,35 @@
+/* 
+    Hi Jasmine,
+
+    File looks good except for one issue. I recommend logging db to see what it is. Right now the server is
+    crashing becuase db.Trip is undefined. Use postman to send a request to the server. This bug is affecting
+    both the create and the findAll methods. 
+    
+    -John
+
+    Postman Request:
+        POST Url: http://localhost:3001/api/trips/new
+        Post Body (raw JSON):
+            {
+                "tripName": "Continental Culture",
+                "placeIds": [],
+                "ownerId": "test1"
+            }
+*/
 const db = require( '../models/trips' );
 
 // Defining methods for the tripsController
 module.exports = {
     'create': function ( req, res ) {
+        // Inspect db and db.trip are by using the console. Send a request with postman to run the code.
+        // console.log( '-------------------------------------------------------------\ndb:\n',
+        //     db,
+        //     '\n-------------------------------------------------------------'
+        // );
+        // console.log( '-------------------------------------------------------------\ndb.Trip:\n',
+        //     db.Trip,
+        //     '\n-------------------------------------------------------------'
+        // );
         db.Trip
             .create( req.body )
             .then( dbModel => res.json( dbModel ) )
