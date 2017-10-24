a53389cfa2da56ccaca3a96f8c04a66a18ad37ce
teddylockhart <teddy.lockhart@gmail.com>
2017-10-23 17:50:14 -0700

    Components Reconstruction

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
+import Wrapper from "./components/Wrapper";
+import Banner from "./components/Banner";
+import Corkboard from "./components/Corkboard";
+
+import { Button, Icon, SideNav, SideNavItem, Footer } from 'react-materialize';
+
 import PlacesSearchComponent from './placesSearch/PlacesSearchContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SideNav
+          trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{ closeOnClick: true }}
+        >
+          <SideNavItem userView
+            user={{
+              background: 'img/office.jpg',
+              image: 'img/yuna.jpg',
+              name: 'John Doe',
+              email: 'jdandturk@gmail.com'
+            }}
+          />
+          <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+          <SideNavItem href='#!second'>Second Link</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>Subheader</SideNavItem>
+          <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+        </SideNav>
         <PlacesSearchComponent />
       </div>
     );
---
+++ client/src/components/Corkboard/Corkboard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Japan from "./japanPH.png"
+
+const Corkboard = () =>
+  <div className="container">
+    <div className="row">
+      <div className="col s12 m6">
+        <div className="card">
+          <div className="card-image">
+            <img src={Japan} alt="japan"/>
+            <span className="card-title">Card Title</span>
+            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+          </div>
+          <div className="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+export default Corkboard;
