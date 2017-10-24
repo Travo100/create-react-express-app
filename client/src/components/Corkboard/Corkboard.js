c6b334770e96aba9f49b03ecd137a9c2abb404d1
Unknown <teddy.lockhart@gmail.com>
2017-10-23 19:59:51 -0700

    CSS Changes before Break

--- client/src/components/Corkboard/Corkboard.js
+++ client/src/components/Corkboard/Corkboard.js
@@ -1,90 +1,93 @@
 import React from "react";
 import Japan from "./japanPH.png"
+import "./Corkboard.css";
 
 const Corkboard = () =>
-  <div className="container">
-    <div className="row">
+  <div className="background-image">
+    <div className="container">
+      <div className="row">
 
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
-        </div>
-      </div>
-      
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
           </div>
         </div>
-      </div>
-      
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
-        </div>
-      </div>
-      
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
           </div>
         </div>
-      </div>
-      
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
-        </div>
-      </div>
-      
-      <div className="col s12 m4">
-        <div className="card">
-          <div className="card-image">
-            <img src={Japan} alt="japan"/>
-            <span className="card-title">Card Title</span>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
-          </div>
-          <div className="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
           </div>
         </div>
-      </div>
+        
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="col s12 m4">
+          <div className="card">
+            <div className="card-image">
+              <img src={Japan} alt="japan"/>
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">clear</i></a>
+            </div>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+        </div>
 
+      </div>
     </div>
-  </div>
 
+  </div>
 
 export default Corkboard;
