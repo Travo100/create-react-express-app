7cba550de9d6147fa8c76e260cb7674916d88396
John Desrosiers <johndrunner@yahoo.com>
2017-10-24 21:08:37 -0700

    display About view on root

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
-import Dashboard from "./components/Dashboard";
+import Dashboard from './components/Dashboard';
 
 // test about component
 import About from './components/LoginPage/About/About.js';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact={true} path="/dashboard" component={Dashboard}/> 
-        </div>
-      </Router>
-    );
-  }
+    render () {
+        return (
+            <Router>
+                <div>
+                    <Route exact={true} path="/" component={About} />
+                    <Route exact={true} path="/dashboard" component={Dashboard} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
